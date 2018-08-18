d9a63528e76687abb4ce8725ee463c3e92240dc6
Katy <ihatetoast@gmail.com>
2018-08-18 10:59:55 -0500

    set up files and got gulp up and running.

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+
+var cssnano = require('gulp-cssnano');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify-es').default;
+
+gulp.task('sass', function() {
+  return gulp
+    .src(['app/styles/reset.css', 'app/styles/style.scss'])
+    .pipe(concat('style.css'))
+    .pipe(sass())
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/css'));
+});
+
+//if there are plugins, etc.
+gulp.task('js', function() {
+  return gulp
+    .src(['app/js/plugins/*.js', 'app/js/*.js'])
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('app/styles/*.scss', ['sass']);
+  gulp.watch('app/js/**/*.js', ['js']);
+});
+
+gulp.task('default', ['sass', 'js', 'watch']);
